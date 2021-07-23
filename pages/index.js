355ee012057ea17bfbfb30706f3b7772dec98701
qt-coder <chuchenkhang@gmail.com>
2021-07-23 12:52:31 +0800

    Added meta tags and title

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Splittter | Split The Bill</title>
+        <meta name='description' content='Splittter | Split The Bill'/>
+        <meta name="author" content="QT Coder"/>
+      </Head>
       <div className={styles.container}>
         <Title />
         <Calculator/>
